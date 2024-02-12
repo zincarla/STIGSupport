ddef2522a20466b94c171f5b08fe9c07e6952999
ZiviZ <tal-skala@ziviz.us>
2024-02-12 01:03:31 -0500

    update for cklb support

--- Utility/Set-NRtoOpen.ps1
+++ Utility/Set-NRtoOpen.ps1
@@ -1,17 +1,17 @@
 ﻿<#
 .SYNOPSIS
-    Sets all NotReviewed in a CKL to Open
+    Sets all NotReviewed in a CKLB to Open
 
 .DESCRIPTION
-    Loads a CKL, and sets all Not_Reviewed to Open, then saves it
+    Loads a CKLB, and sets all not_reviewed to open, then saves it
 
 .PARAMETER CKLPath
-    Full path to the CKL file
+    Full path to the CKLB file
   
 .EXAMPLE
-    "Set-NRtoOpen.ps1" -CKLPath 'C:\CKLs\MyChecklist.ckl'
+    "Set-NRtoOpen.ps1" -CKLBPath 'C:\CKLs\MyChecklist.cklb'
 #>
-Param([Parameter(Mandatory=$true)][ValidateScript({Test-Path -Path $_})][string]$CKLPath)
+Param([Parameter(Mandatory=$true)][ValidateScript({Test-Path -Path $_})][string]$CKLBPath)
 
 #Check if module imported
 if ((Get-Module|Where-Object -FilterScript {$_.Name -eq "StigSupport"}).Count -le 0)
@@ -22,14 +22,14 @@ if ((Get-Module|Where-Object -FilterScript {$_.Name -eq "StigSupport"}).Count -l
 }
 
 #If pointing to a single CKL, set children to an array that only contains that one ckl
-if ($CKLPath.EndsWith(".ckl"))
+if ($CKLBPath.EndsWith(".cklb"))
 {
-    $Children = @($CKLPath)
+    $Children = @($CKLBPath)
 }
 else
 {
     #Otherwise, load all CKL files from that path and put it into an array
-    $Files = Get-ChildItem -Path $CKLPath -Filter "*.CKL"
+    $Files = Get-ChildItem -Path $CKLBPath -Filter "*.CKLB"
     $Children = @()
     foreach ($File in $Files)
     {
@@ -37,39 +37,39 @@ else
     }
     if ($Children.Length -eq 0)
     {
-        Write-Error "No CKL files found in directory"
+        Write-Error "No CKLB files found in directory"
         return
     }
 }
 
 $I=0
-Write-Progress -Activity "Setting CKLs" -PercentComplete (($I*100)/$Children.Length) -Id 1
+Write-Progress -Activity "Setting CKLBs" -PercentComplete (($I*100)/$Children.Length) -Id 1
 #Loop through the CKL Files
 foreach ($Child in $Children)
 {
     $Name = $Child.Split("\")
     $Name = $Name[$Name.Length-1]
-    Write-Progress -Activity "Setting CKLs" -PercentComplete (($I*100)/$Children.Length) -Id 1
+    Write-Progress -Activity "Setting CKLBs" -PercentComplete (($I*100)/$Children.Length) -Id 1
     #Load the CKL file
-    $CKLData = Import-StigCKL -Path $Child
+    $CKLBData = Import-StigCKLBFile -Path $Child
     Write-Progress -Activity "$Name" -Status "Loading Stigs" -PercentComplete 0 -Id 2
     #Load the stig results from the CKL
-    $Stigs = Get-VulnCheckResult -XMLData $CKLData
-    #For each stig, that is "Not_Reviewed", set it to open
+    $Stigs = Get-StigCKLBRuleInfo -CKLBData $CKLBData -All
+    #For each stig, that is "not_reviewed", set it to open
     Write-Progress -Activity "$Name" -Status "Starting Loop" -PercentComplete (($I*100)/$Children.Length) -Id 2
     $S =0
     foreach ($Stig in $Stigs)
     {
-        Write-Progress -Activity "$Name" -Status "$($Stig.VulnID)" -PercentComplete (($S*100)/$Stigs.Length) -Id 2
-        if ($Stig.Status -eq "Not_Reviewed" -or $Stig.Status -eq "NotReviewed")
+        Write-Progress -Activity "$Name" -Status "$($Stig.group_id)" -PercentComplete (($S*100)/$Stigs.Length) -Id 2
+        if ($Stig.Status -eq "not_reviewed" -or $Stig.Status -eq "NotReviewed")
         {
-            Write-Host "$($Stig.VulnID) is being marked open"
-            Set-VulnCheckResult -XMLData $CKLData -VulnID $Stig.VulnID -Result Open
+            Write-Host "$($Stig.group_id) is being marked open"
+            Set-StigCKLBRuleFinding -CKLBData $CKLBData -VulnID $Stig.group_id -Result open
         }
         $S++
     }
     #Save the ckl
-    Export-StigCKL -XMLData $CKLData -Path $Child
+    Export-StigCKLBFile -CKLBData $CKLBData -Path $Child
     Write-Progress -Activity "$Name" -Status "Complete" -PercentComplete 100 -Id 2 -Completed
     $I++
 }
